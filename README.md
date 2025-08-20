f9b3c2657f22a45b103e7816efcf76635e19ed7c
EmilianoGR96 <emgrcr96@gmail.com>
2025-08-20 00:35:16 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 Este es el proyecto del sprint 7, este sprint fue sobre herramientas de desarrollo de software. EL proyecto incluye la creación y gestión de entornos virtuales de Python y el desarrollo de una aplicación web.
 
 
-
 Especificamente en este proyecto el usuario puede escoger crear un histograma o diagrama de dispersion con los datos de los vehiculos en el dataset.
-
-https://sprint7-unhr.onrender.com
---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st 
+import plotly.express as px 
+
+car_data = pd.read_csv('/Users/PC/Desktop/Sprint7/Sprint7/vehicles_us.csv')
+
+hist_button = st.checkbox('Construir histograma') # crear un botón
+     
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = st.checkbox('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x='odometer', y='price', labels={'odometer' : 'Odómetro', 'price' : 'Precio'})
+    st.plotly_chart(fig, use_container_width=True)       
